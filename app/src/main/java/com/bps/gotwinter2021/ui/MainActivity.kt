bbda4388f3ab9e90b1dfb2f928ea7d508472fd0a
Inasity <lifepunch@hotmail.com>
2021-11-01 16:04:17 -0500

    feature/BPSINT-4-Bottom-Navigation Implementation

--- app/src/main/java/com/bps/gotwinter2021/ui/MainActivity.kt
+++ app/src/main/java/com/bps/gotwinter2021/ui/MainActivity.kt
@@ -2,11 +2,22 @@ package com.bps.gotwinter2021.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import com.bps.gotwinter2021.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+
+        val navController = navHostFragment.navController
+
+        bottomNavigationView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/bps/gotwinter2021/ui/homescreen/HomeScreenFragment.kt
+++ app/src/main/java/com/bps/gotwinter2021/ui/homescreen/HomeScreenFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import com.bps.gotwinter2021.R
 
-class HomeScreen : Fragment() {
+class HomeScreenFragment : Fragment() {
 
 
 
