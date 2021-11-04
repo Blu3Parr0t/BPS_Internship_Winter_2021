c2e053aab423f5eff0bf833a91a8569f3dd7d766
Rafa311018 <rafael311018@gmail.com>
2021-11-04 09:47:41 -0600

    card_item_view implemented

--- app/src/main/java/com/bps/gotwinter2021/ui/homescreen/HomeScreen.kt
+++ app/src/main/java/com/bps/gotwinter2021/ui/homescreen/HomeScreen.kt
@@ -8,14 +8,17 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.bps.gotwinter2021.R
+import com.bps.gotwinter2021.common.createViewModel
 import com.bps.gotwinter2021.databinding.FragmentHomeScreenBinding
 import com.bps.gotwinter2021.ui.homescreen.adapter.HousesAdapter
 
 class HomeScreen : Fragment() {
-
+    private val viewModel: HomeScreenViewModel by lazy {
+        createViewModel { HomeScreenViewModel(app = this.requireActivity().application) }
+    }
     private lateinit var binding: FragmentHomeScreenBinding
     private lateinit var adapter: HousesAdapter
-    private val houses: List<String> = listOf("Stark","Lannister ","Targaryen","Baratheon ","Greyjoy","Martell","Tully","Arryn","Tyrel")
+    private val houses: List<String> = listOf("Stark","Lannister","Targaryen","Baratheon","Greyjoy","Martell","Tully","Arryn","Tyrel")
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,6 +26,7 @@ class HomeScreen : Fragment() {
     ): View? {
         binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home_screen, container, false)
         binding.lifecycleOwner = this
+        binding.viewModel = viewModel
         adapter = HousesAdapter()
 
         adapter.setData(houses)
