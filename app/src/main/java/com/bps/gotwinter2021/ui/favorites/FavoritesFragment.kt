69f0fec669960bea51aab476e87089acb0faece1
jjhuert99 <jahuert99@gmail.com>
2021-11-03 20:03:08 -0500

    added some elements to favorites fragment UI

--- app/src/main/java/com/bps/gotwinter2021/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/com/bps/gotwinter2021/ui/favorites/FavoritesFragment.kt
@@ -1,32 +1,32 @@
 package com.bps.gotwinter2021.ui.favorites
 
-import androidx.lifecycle.ViewModelProvider
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.bps.gotwinter2021.R
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.bps.gotwinter2021.databinding.FavoritesFragmentBinding
+import jp.wasabeef.blurry.Blurry
 
 class FavoritesFragment : Fragment() {
-
-    companion object {
-        fun newInstance() = FavoritesFragment()
+    private val viewModel: FavoritesViewModel by lazy{
+        ViewModelProvider(this).get(FavoritesViewModel::class.java)
     }
 
-    private lateinit var viewModel: FavoritesViewModel
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val binding = FavoritesFragmentBinding.inflate(inflater)
+        binding.setLifecycleOwner(this)
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.favorites_fragment, container, false)
+        Blurry.with(requireContext())
+            .radius(10)
+            .sampling(8)
+            .color(Color.argb(66, 255, 255, 0))
+            .async()
+            .animate(500)
+            .onto(binding.wholeThing);
+
+        return binding.root
     }
-
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(FavoritesViewModel::class.java)
-        // TODO: Use the ViewModel
-    }
-
 }
--- app/src/main/java/com/bps/gotwinter2021/ui/homescreen/HomeScreen.kt
+++ app/src/main/java/com/bps/gotwinter2021/ui/homescreen/HomeScreen.kt
@@ -1,12 +1,11 @@
 package com.bps.gotwinter2021.ui.homescreen
 
 import android.os.Bundle
-import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.bps.gotwinter2021.R
 import com.bps.gotwinter2021.databinding.FragmentHomeScreenBinding
 import com.bps.gotwinter2021.ui.homescreen.adapter.HousesAdapter
