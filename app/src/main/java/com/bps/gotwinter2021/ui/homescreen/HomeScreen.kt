4bdbee42bfe464da0a21f095cb8440706dc2b958
Rafa311018 <rafael311018@gmail.com>
2021-11-12 11:05:04 -0600

    implement DI to HomeScreen, and missing DI to SearchResultsViewModel

--- app/src/main/java/com/bps/gotwinter2021/ui/homescreen/HomeScreen.kt
+++ app/src/main/java/com/bps/gotwinter2021/ui/homescreen/HomeScreen.kt
@@ -1,20 +1,22 @@
 package com.bps.gotwinter2021.ui.homescreen
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import com.bps.gotwinter2021.R
 import com.bps.gotwinter2021.common.createViewModel
-import com.bps.gotwinter2021.data.network.repo.GOTRepo
 import com.bps.gotwinter2021.databinding.FragmentHomeScreenBinding
-import com.bps.gotwinter2021.favorites.database.FavoriteDatabase
 import com.bps.gotwinter2021.ui.homescreen.adapter.HousesAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeScreen : Fragment() {
+
     private lateinit var binding: FragmentHomeScreenBinding
     private lateinit var adapter: HousesAdapter
     private val houses: List<String> = listOf(
@@ -33,14 +35,8 @@ class HomeScreen : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        val viewModel: HomeScreenViewModel by viewModels()
         val application = requireNotNull(this.activity).application
-        val viewModel: HomeScreenViewModel by lazy {
-            createViewModel {
-                HomeScreenViewModel(
-                    application
-                )
-            }
-        }
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home_screen, container, false)
         binding.lifecycleOwner = this
